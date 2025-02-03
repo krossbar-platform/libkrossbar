7accce3a63a0a4391d30e88b9462eef897d09c58
Alexander Smoktal <cosm.ua@gmail.com>
2025-02-03 15:23:56 +0200

    Implement some of the UDS managing

--- src/uds/transport_uds.c
+++ src/uds/transport_uds.c
@@ -3,9 +3,12 @@
 #include <sys/socket.h>
 #include <fcntl.h>
 
+#include <mpack-writer.h>
+#include <utlist.h>
+
 #include "message_writer_uds.h"
 
-kb_transport_t *transport_uds_init(const char *name, int fd, size_t max_message_size, struct io_uring *ring)
+kb_transport_t *transport_uds_init(const char *name, int fd, size_t max_message_size, size_t max_buffered_messages)
 {
     kb_transport_uds_t *transport = calloc(1, sizeof(kb_transport_uds_t));
     if (transport == NULL)
@@ -16,6 +19,8 @@ kb_transport_t *transport_uds_init(const char *name, int fd, size_t max_message_
 
     transport->name = strdup(name);
     transport->max_message_size = max_message_size;
+    transport->max_buffered_messages = max_buffered_messages;
+    transport->sock_fd = fd;
 
     transport->base.message_init = transport_uds_message_init;
     transport->base.message_receive = transport_uds_message_receive;
@@ -32,6 +37,11 @@ kb_message_writer_t *transport_uds_message_init(kb_transport_t *transport)
 {
     kb_transport_uds_t *self = (kb_transport_uds_t *)transport;
 
+    if (self->out_message_count >= self->max_buffered_messages)
+    {
+        return NULL;
+    }
+
     char *buffer = malloc(self->max_message_size);
     if (!buffer)
     {
@@ -46,10 +56,113 @@ kb_message_writer_t *transport_uds_message_init(kb_transport_t *transport)
 
 int transport_uds_message_send(kb_transport_t *transport, kb_message_writer_t *writer)
 {
+    kb_transport_uds_t *self = (kb_transport_uds_t *)transport;
+    kb_message_writer_uds_t *writer_uds = (kb_message_writer_uds_t *)writer;
+
+    size_t message_size = writer_message_size(writer);
+    char *data = writer->data_writer->buffer;
+
+    // Shrink message to fre some extra memory
+    realloc(data, message_size);
+    out_messages_t *out_message = calloc(1, sizeof(out_messages_t));
+    out_message->message.data = data;
+    out_message->message.data_size = message_size;
+
+    DL_APPEND(self->out_messages, out_message);
+
+    self->out_message_count++;
 }
 
-kb_message_t *transport_uds_message_receive(kb_transport_t *transport);
-int transport_uds_message_release(kb_transport_t *transport, kb_message_t *message);
+kb_message_t *transport_uds_message_receive(kb_transport_t *transport)
+{
 
-int transport_uds_get_fd(kb_transport_t *transport);
-void transport_uds_destroy(kb_transport_t *transport);
+    kb_transport_uds_t *self = (kb_transport_uds_t *)transport;
+
+    // Receiving a new message
+    if (self->in_message.data == NULL)
+    {
+        uint64_t read_size = 0;
+
+        // Check if we can read message size
+        if (recv(self->sock_fd, &read_size, sizeof(read_size), MSG_PEEK) != sizeof(uint64_t))
+        {
+            return NULL;
+        }
+
+        recv(self->sock_fd, &read_size, sizeof(read_size), 0);
+        self->in_message.data_size = read_size;
+        self->in_message.current_offset = 0;
+        self->in_message.data = malloc(read_size);
+    }
+
+    ssize_t bytes_received = recv(self->sock_fd,
+                                  self->in_message.data + self->in_message.current_offset,
+                                  self->in_message.data_size - self->in_message.current_offset,
+                                  0);
+
+    if (bytes_received > 0)
+    {
+        self->in_message.current_offset += bytes_received;
+
+        if (self->in_message.current_offset == self->in_message.data_size)
+        {
+            kb_message_t *message = malloc(sizeof(kb_message_t));
+            message_init(message, self->in_message.data, self->in_message.data_size);
+
+            return message;
+        }
+    }
+
+    return NULL;
+}
+
+int transport_uds_message_release(kb_transport_t *transport, kb_message_t *message)
+{
+    kb_transport_uds_t *self = (kb_transport_uds_t *)transport;
+
+    free(self->in_message.data);
+    self->in_message.data = NULL;
+    self->in_message.data_size = 0;
+
+    return 0;
+}
+
+int transport_uds_get_fd(kb_transport_t *transport)
+{
+    kb_transport_uds_t *self = (kb_transport_uds_t *)transport;
+
+    return self->sock_fd;
+}
+
+void transport_uds_destroy(kb_transport_t *transport)
+{
+    kb_transport_uds_t *self = (kb_transport_uds_t *)transport;
+
+    if (self->sock_fd != 0)
+    {
+        close(self->sock_fd);
+    }
+
+    if (self->name != NULL)
+    {
+        free(self->name);
+    }
+
+    if (self->out_messages != NULL)
+    {
+        out_messages_t *out_message, *tmp;
+        DL_FOREACH_SAFE(self->out_messages, out_message, tmp)
+        {
+            DL_DELETE(self->out_messages, out_message);
+            free(out_message->message.data);
+            free(out_message);
+        }
+    }
+
+    if (self->in_message.data != NULL)
+    {
+        free(self->in_message.data);
+    }
+
+    free(self);
+}
--- src/uds/transport_uds.h
+++ src/uds/transport_uds.h
@@ -4,18 +4,44 @@
 
 struct io_uring;
 
+struct message_buffer_s
+{
+    char *data;
+    size_t data_size;
+    size_t current_offset;
+};
+
+typedef struct message_buffer_s message_buffer_t;
+
+struct out_messages_s
+{
+    message_buffer_t message;
+    struct out_messages_s *prev; /* needed for a doubly-linked list only */
+    struct out_messages_s *next; /* needed for singly- or doubly-linked lists */
+};
+
+typedef struct out_messages_s out_messages_t;
+
 struct kb_transport_uds_s
 {
     kb_transport_t base;
     struct io_uring *ring;
     const char *name;
-    int shm_fd;
+    int sock_fd;
     size_t max_message_size;
+    // Out messages for writing into the socket
+    out_messages_t *out_messages;
+    // Current number of buffered messages
+    size_t out_message_count;
+    // Max number of buffered messages
+    size_t max_buffered_messages;
+    // Incoming message buffer
+    message_buffer_t in_message;
 };
 
 typedef struct kb_transport_uds_s kb_transport_uds_t;
 
-kb_transport_t *transport_uds_init(const char *name, int fd, size_t max_message_size, struct io_uring *ring);
+kb_transport_t *transport_uds_init(const char *name, int fd, size_t max_message_size, size_t max_buffered_messages);
 
 kb_message_writer_t *transport_uds_message_init(kb_transport_t *transport);
 int transport_uds_message_send(kb_transport_t *transport, kb_message_writer_t *writer);
